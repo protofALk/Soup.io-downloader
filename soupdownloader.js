46c32650d14f0de0e8420f59463ba622eb5fba3e
fALk Gärtner <falk@prototypen.com>
2013-02-07 13:59:43 +0100

    cleanup and commenting

--- soupdownloader.js
+++ soupdownloader.js
@@ -5,15 +5,18 @@ var fs = require('fs'),
 
 var count = 0,
 	countsearch = 0,
-	stream = fs.createReadStream('./soup_falk_2013-01-03.rss'),
-	linechunks = [];
+	stream = fs.createReadStream('./soup_falk_2013-01-03.rss');
 
 var parralelDLs = 2; // how many pictures will be downloaded in parralel - adjust to soup speed of the day and you connection speed
 
 
+// take apart the rsscode line 
+// if there is a <soup:attributes> tag 
+// and inside that (JSON object) a url field
+// start the downloading madness 
+// pass down the callback from the queue
 
 function downloader(task, callback){
-
  	var soupline = task.line.toString("utf8").match("<soup:attributes>(.*?)</soup:attributes>");
  	if (soupline != null) {
  		var souplineOBJ = JSON.parse(soupline[1]);
@@ -30,77 +33,34 @@ function downloader(task, callback){
   	};
 };
 
-
-
+// initialize the process and download queue 
+// 
 var q = async.queue(function (task, callback) {
     console.log('hello ' + task.id);
    	downloader(task, callback);
 }, parralelDLs);
 
-function test(line){
+//push a new line into the queue to be processed 
+function processRssLine(line){
 	count++;
 	q.push({id: count.toString(), line: line}, function(err){
 		if (err) console.log("ERROR in Queue: ", err.message);
 	});
 };
 
+// if the queue gets empty resume the filestream 
 q.empty = function(){
 	console.log("NEW BATCH");
 	stream.resume();
 };
 
-
-
-// stream.setEncoding('utf8');
-// stream.addListener('data', function (line) {
-//     // pause stream
-//     stream.pause();
-//     // make async API call...
-//     test(line, function(message) {
-//         // then resume to process next line
-//         console.log(message);
-//         stream.resume();
-//     });
-// })
-
-
-// async.forEachSeries(new lazy(stream).lines, function(callback){
-// 	console.log("linefinder");
-// 	callback ("done");
-// }, function(err){ if (err) throw err;});
+// read in the file in chunks process asynchronous as much as possible 
+// pause after each chunk cause downloading needs to catch up 
 
 new lazy(stream)
     .lines
     .forEach(function(line){
     	stream.pause();
-    	// at this point  we already have lots of entries 
-    	// so we would likely pause the stream until we get through all these 
-		// lets call the function that brings us to the one by one line appoach
-        test(line.toString());
+        processRssLine(line.toString());
     }
 );
-
-
-
-
-
-// input.on('data', function(line) {
-// 	count++;
-// 	//console.log(line.toString("utf8").match("<soup:attributes>(.*?)</soup:attributes>"));
-// 	var soupline = line.toString("utf8").match("<soup:attributes>(.*?)</soup:attributes>");
-// 	if (soupline != null) {
-// 		var souplineOBJ = JSON.parse(soupline[1]);
-//   		//console.log(souplineOBJ);
-//   		if ("url" in souplineOBJ){
-// 			console.log(souplineOBJ.url);
-//   			//test(souplineOBJ.url, function(message){
-//   			//});
-//   		}
-//   		countsearch++;
-
-//   }
-// });
-
-// input.on('end', function(){
-// 	console.log("Count: " + count.toString(), "countsearch: " + countsearch.toString());
-// })
